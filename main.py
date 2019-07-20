3deef466112233ae2af9e87ff67a6ba9cc96f8ae
Leeelooo <leeeloooda@gmail.com>
2019-07-21 02:05:34 +0500

    dodique_output.prettify

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'http://archillect.com'
+URL = 'http://archillect.com'
 
 
 def items_fetching_recursive(item_id, take_first):
     if take_first > 0 and item_id > 0:
-        item_url = url + '/' + str(item_id)
+        item_url = URL + '/' + str(item_id)
         print(item_url)
         item_soup = BeautifulSoup(requests.get(item_url).content, 'lxml')
         sources = list(map(lambda x: x['href'], item_soup.find(id='sources').find_all('a')))
+        sources[0] = sources[0][56:]
         print('\t' + str(sources))
         items_fetching_recursive(item_id - 1, take_first - 1)
 
 
-soup = BeautifulSoup(requests.get(url).content, 'lxml')
+soup = BeautifulSoup(requests.get(URL).content, 'lxml')
 last_id = int(soup.find('div', 'overlay').string.strip())
 items_fetching_recursive(int(last_id), 1)
