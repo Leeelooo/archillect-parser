46098eca11a7b3994cecc8ae8d853c4a5d645eb1
Leeelooo <leeeloooda@gmail.com>
2019-07-21 13:52:33 +0500

    dodique_output.prettify

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ def items_fetching_recursive(item_id, take_first):
         print(item_url)
         item_soup = BeautifulSoup(requests.get(item_url).content, 'lxml')
         sources = list(map(lambda x: x['href'], item_soup.find(id='sources').find_all('a')))
-        sources[0] = sources[0][56:] #skipping google image search to get link to source
-        print('\t' + str(sources))
+        # item_tags_soup = BeautifulSoup(requests.get(sources[0]).content, 'lxml') somehow get tags created by google search
+        sources[0] = sources[0][56:]  # skipping google image search to get link to source
+        print('└───' + str(sources))
+        # print(item_tags_soup.prettify())
         items_fetching_recursive(item_id - 1, take_first - 1)
 
 
 soup = BeautifulSoup(requests.get(URL).content, 'lxml')
 last_id = int(soup.find('div', 'overlay').string.strip())
-items_fetching_recursive(int(last_id), 1)
+items_fetching_recursive(int(last_id), 10)
