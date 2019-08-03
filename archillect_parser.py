f4b377dd6397a7a010c14bb824cbcda265b1a017
Leeelooo <leeeloooda@gmail.com>
2019-08-04 00:15:29 +0500

    some structure (still bad)

--- archillect_parser.py
+++ archillect_parser.py
@@ -1,10 +1,7 @@
 from bs4 import BeautifulSoup
-from item_model import ArchillectItem
-from requests import put
+from utils import get_arg_value
 import requests
 import sys
-import json
-from utils import get_arg_value
 
 URL = 'http://archillect.com'
 
@@ -25,6 +22,7 @@ def fetch_items(item_id, items_count):
 
     return items_list
 
+
 def get_last_items(items_count=10):
     soup = BeautifulSoup(requests.get(URL).content, 'lxml')
     last_id = int(soup.find('div', 'overlay').string.strip())
@@ -36,8 +34,8 @@ def main():
     items_to_parse = get_arg_value(sys.argv[1:], 'items')
     items = get_last_items(int(items_to_parse) if items_to_parse.isdigit() else 10)
     for item in items:
-        response = put('http://localhost:5000/items/', data={'item_id': item[0], 'sources': json.dumps(item[1])})
-        print(response.json())
+        print(item[0])
+        print('└───' + str(item[1]))
 
 
 if __name__ == '__main__':
